7e717401dd5c05127beff4cb7774071d6717f81f
Jaz Gamarra <111554355+jazgamarra@users.noreply.github.com>
2022-10-17 09:50:02 -0300

    add app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests, json 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url='https://ghibliapi.herokuapp.com/Films/'
+    respuesta = requests.get(url)
+    peliculas = json.loads(respuesta.text) 
+    return render_template("index.html", peliculas=peliculas) 
+
+app.run(debug=True)   
